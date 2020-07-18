4e6241a4295f55bcca4c11f746b255fcb1bfec07
rmodali365 <56807188+rmodali365@users.noreply.github.com>
2020-07-18 15:03:08 -0700

    Update firedetection.py

--- firedetection.py
+++ firedetection.py
@@ -114,39 +114,3 @@ for fn in uploaded.keys():
   else:
     print(fn + " is a fire")
 
-from keras.models import load_model
-from keras.preprocessing import image
-img_width, img_height = 300, 300
-
-# load the model we saved
-model = load_model('model.fire')
-model.compile(loss='binary_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
-
-# predicting images
-img = image.load_img('fire.jpg', target_size=(img_width, img_height))
-x = image.img_to_array(img)
-x = np.expand_dims(x, axis=0)
-
-images = np.vstack([x])
-classes = model.predict_classes(images, batch_size=10)
-print (classes)
-
-# predicting multiple images at once
-img = image.load_img('fire.jpg', target_size=(img_width, img_height))
-y = image.img_to_array(img)
-y = np.expand_dims(y, axis=0)
-
-# pass the list of multiple images np.vstack()
-images = np.vstack([x, y])
-classes = model.predict_classes(images, batch_size=10)
-
-# print the classes, the images belong to
-print (classes)
-print (classes[0])
-print (classes[0][0])
-
-"""110 fire images
-157 normal images
-"""
